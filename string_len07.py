6d4198b2fc68d521e25607b1568c42470bbf0edb
shaxrizodaisoyeva <shaxrizodaisoyeva24@gmail.com>
2023-01-04 08:57:24 +0500

    7

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,15 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    x=[]
+    if len(s1)%2==1:
+        x = s1
+    if len(s2)%2==1:
+        x += ", "+s2
+    if len(s3)%2==1:
+        x += ", "+s3
+    return [x]
+s1 = "abcdi"
+s2 = "efjhijk"
+s3 = "aziz"
+print(main(s1,s2,s3))
